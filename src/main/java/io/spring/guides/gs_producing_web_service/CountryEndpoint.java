d5b3ea9228bd69a2d5cdc8ff0929c77153e95e3d
tharinduEranga <tharindueranga3@gmail.com>
2021-06-14 15:47:38 +0530

    added URL attribute

--- src/main/java/io/spring/guides/gs_producing_web_service/CountryEndpoint.java
+++ src/main/java/io/spring/guides/gs_producing_web_service/CountryEndpoint.java
@@ -1,5 +1,7 @@
 package io.spring.guides.gs_producing_web_service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
@@ -13,7 +15,7 @@ import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 @Endpoint
 public class CountryEndpoint {
     private static final String NAMESPACE_URI = "http://spring.io/guides/gs-producing-web-service";
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(CountryEndpoint.class);
     private CountryRepository countryRepository;
 
     @Autowired
@@ -24,9 +26,16 @@ public class CountryEndpoint {
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
     @ResponsePayload
     public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
+        LOGGER.info("URL Is: {}", request.getUrl());
         GetCountryResponse response = new GetCountryResponse();
         response.setCountry(countryRepository.findCountry(request.getName()));
 
         return response;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "urlReq")
+    @ResponsePayload
+    public String getCountry(@RequestPayload String request) {
+        return request.toUpperCase();
     }
 }
